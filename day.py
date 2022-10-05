09eada5e0a98a1c4a5d4b73c15657f904bd8c562
geoperros <geoperros@hotmail.com>
2022-10-05 23:45:11 +0300

    Need to fix customer_del func

--- day.py
+++ day.py
@@ -1,12 +1,13 @@
 import csv
 from datetime import date
 current_date = date.today()
-current_file_name = ''
-def csv_log_day(current_date, current_name, current_product, current_value, current_file_name):
-    with open(current_file_name, 'a', newline = '') as new_file:
-        fieldnames = ['ΗΜ/ΝΙΑ', 'ΟΝΟΜΑ', 'ΕΙΔΟΣ','ΠΟΣΟΤΗΤΑ']
-        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
-        csv_writer.writerow({'ΗΜ/ΝΙΑ': current_date, 'ΟΝΟΜΑ': current_name, 'ΕΙΔΟΣ' : current_product , 'ΠΟΣΟΤΗΤΑ' : current_value})
+name_del_out = '1'
+csv_lines = dict()
+def csv_file_creation():
+    with open('customers.csv', 'a', newline ='') as new_file:
+        fieldnames = ['name' , 'value']
+        csv_writer = csv.DictWriter(new_file, fieldnames = fieldnames)
+        csv_writer.writeheader()
 
 def customer_choice(name, value):
     with open('customers.csv', 'a', newline = '') as new_file:
@@ -14,19 +15,31 @@ def customer_choice(name, value):
         csv_writer = csv.DictWriter(new_file, fieldnames = fieldnames)
         csv_writer.writerow({'name': name,'value':value})
 
+def customer_del(name_del_inp,name_del_out):
+    with open('customers.csv', 'r',newline='') as new_file:
+        csv_reader = csv.DictReader(new_file)
+        for row in csv_reader:
+            if row['name'] not in name_del_inp:
+                csv_lines.append(row['name'],row['value'])
+
+    with open('customers.csv', 'a', newline='') as new_file_w:
+        fieldnames = ['name' , 'value']
+        csv_writer = csv.DictWriter(new_file_w, fieldnames=fieldnames)
+        csv_writer.writeheader
+        csv_writer.writerows(csv_lines)
+            
 while True:
-    choice = input('"1" Επιλογη ονόματος αρχείου. "2" Καταχώρηση πελάτη και είδους. "3" Έξοδος: ')
+    choice = input(' "1" Καταχώρηση πελάτη και είδους. "2" Διαγραφή πελάτη "3" Δημειουργεία αρχείου "4" Έξοδος : ')
     if choice == '1':
-        current_file_name = input('Εισάγετε όνομα αρχείου : ')
-    elif choice == '2':
         name = input('Όνομα πελάτη :')
         value = input('Είδος :')
         customer_choice(name,value)
+    elif choice == '2':
+        name_del_inp = input('Πελάτης προς διαγραφή : ')
+        customer_del(name_del_inp,name_del_out)
 
-        
+    elif choice == '3':
+        csv_file_creation()
 
-
-
-
-    elif choice == "3":
+    elif choice == '4':
         break
